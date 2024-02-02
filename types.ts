1cb5d6ebf3ab4a6604dd3a21e2d4c8590c766c1b
Chad Foster <chad.foster@fgsglobal.com>
2024-02-01 23:44:48 -0500

    Add featured products

--- types.ts
+++ types.ts
@@ -18,5 +18,22 @@ export interface Product {
   isFeatured: boolean;
   size: Size;
   color: Color;
-  images: Image;
+  images: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
 }
